90be651b775b79ba2ab9d482c2c122a9adc97231
Celeste Roselli <72556836+celesteroselli@users.noreply.github.com>
2025-05-23 14:58:50 -0400

    added elevator open art

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var el = document.getElementById("elevator");
 var el_con = document.getElementById("container");
 var el_icon = document.getElementById("elevator-landing-icon");
-var el_text = document.getElementById("elevator-landing-text");
+var start_text = document.getElementById("elevator-landing-text");
 var myError = document.getElementById("error");
+var interior = document.getElementById("interior");
+var body = document.querySelector("body");
 var audio = new Audio("music.mp3"); 
 var ding = new Audio("elevatording.mp3"); 
 var done = false;
+var fadeIn = true;
 
 function elevatorOpen() {
     let start = Date.now(); // remember start time
@@ -62,13 +65,14 @@ function zoom() {
 
     if (timePassed >= 6000) {
         clearInterval(timer); // finish the animation after 2 seconds
-        el_text.style.opacity = "100%";
+        el_con.hidden = true;
         el_icon.style.opacity = "0%";
+        transition();
         return;
     } 
 
     el.style.top = 150 - (timePassed / 2) + "px";
-    el.style.left = 50 - (timePassed / 29) + "%";
+    el.style.left = 50 - (timePassed / 27) + "%";
     el_icon.style.width = 700 + (timePassed / 0.8) + "px";
 
     }, 20);
@@ -124,11 +128,76 @@ function comeUp(timePassed) {
   }
 
 //setups
+el_con.hidden = true;
+el.style.opacity = "0%";
 document.getElementById("button").addEventListener("click", run);
-el_text.style.opacity = "0%";
+start_text.style.opacity = "0%";
 
 function run() {
+    el_con.hidden = false;
     document.getElementById("button").style.opacity = "0%";
+    el.style.opacity = "100%";
     downAnimation();
     audio.play();
+}
+
+function backgroundUpAnimation() {
+    let start = Date.now(); // remember start time
+
+    let timer = setInterval(function() {
+    // how much time passed from the start?
+    let timePassed = Date.now() - start;
+
+    if (timePassed >= 1000) {
+        clearInterval(timer);
+        elevatorOpen()
+
+        return;
+    } 
+
+    // draw the animation at the moment timePassed
+    comeUp(timePassed);
+
+    }, 20);
+}
+
+function transition() {
+    body.style.backgroundColor = "#1d1d2a";
+    let start = Date.now(); // remember start time
+
+    let timer = setInterval(function() {
+    // how much time passed from the start?
+    let timePassed = Date.now() - start;
+
+    if (timePassed >= 1000) {
+        start_text.style.opacity = "100%";
+    } 
+
+    if (timePassed >= 4000) {
+        fadeIn = false;
+    } 
+
+    if (timePassed >= 5000) {
+        clearInterval(timer);
+        start_text.style.opacity = "0%";
+        newScreen();
+
+        return;
+    }
+
+    // draw the animation at the moment timePassed
+    if (fadeIn) {
+    start_text.style.opacity = timePassed / 10 + "%";
+    } else if (!fadeIn && timePassed <= 4800)  {
+        start_text.style.opacity = 100 - ((timePassed-4000) / 10) + "%";
+    }
+
+    }, 20);
+    
+    
+
+}
+
+function newScreen() {
+    interior.style.backgroundImage = "url('interior.png')";
 }
